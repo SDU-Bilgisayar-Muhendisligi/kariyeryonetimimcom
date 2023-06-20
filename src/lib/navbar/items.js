5f1d5cffbb4ce4b099f0a2d8ae1e4602a59a36bf
kgnkync <115173693+kgnkync@users.noreply.github.com>
2023-06-20 21:03:53 +0300

    Delete src dir

--- src/lib/navbar/items.js
+++
@@ -1,8 +0,0 @@
-let items = [
-	{ name: 'İş İlanları', href: '/jobadverts', icon: '' },
-	{ name: 'Gruplar', href: '/group', icon: '' },
-	{ name: 'Etkinlikler', href: '/event', icon: '' },
-	{ name: 'Şirketler', href: '/company', icon: '' },
-];
-
-export default items;
